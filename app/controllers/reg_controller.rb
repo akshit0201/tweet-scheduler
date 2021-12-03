f6f26f703810f029065d44b46c68878f201800de
akshit0201 <akshit0201@gmail.com>
2021-12-03 22:03:12 +0400

    Added Error handling for Signup form

--- app/controllers/reg_controller.rb
+++ app/controllers/reg_controller.rb
@@ -2,4 +2,16 @@ class RegController < ApplicationController
   def new
     @user = User.new
   end
+  def create
+    @user = User.new(user_p)
+    if @user.save
+      redirect_to root_path, notice: "Account Created"
+    else
+      render :new
+    end
+  end
+  private
+  def user_p
+    params.require(:user).permit(:email,:password,:password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "about-us" , to: "about#index" , as: :about
   get "sign-up" , to: "reg#new" , as: :sign_up
+  post "sign-up", to: "reg#create"
   root to: "main#index"
 end
